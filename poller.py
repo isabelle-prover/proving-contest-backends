007afb2e673298aabb532290f2b9a6f0c52513fd
Maximilian PL Haslbeck <haslbema@in.tum.de>
2019-07-05 09:24:38 +0200

    error handling in polling loop

--- poller.py
+++ poller.py
@@ -92,74 +92,78 @@ class Poller(ABC):
         logger = self.logger
         logger.info("entering the polling loop")
         while True:
-            time.sleep(0)
+            time.sleep(2)
             grader_msg = ""
 
             # poll from server
-            logger.debug("poll from server")
+            try:
+                logger.debug("poll from server")
 
-            url = self.baseurl + self.pollurl
+                url = self.baseurl + self.pollurl
 
-            # send get request
-            response = requests.get(url, verify=True, headers=self.headers)
-            logger.debug("sent GET request to " + url)
+                # send get request
+                response = requests.get(url, verify=True, headers=self.headers)
+                logger.debug("sent GET request to " + url)
 
-            # work with answer
-            if response.ok:
-                data = json.loads(response.content)
+                # work with answer
+                if response.ok:
+                    data = json.loads(response.content)
 
-                # no task available
-                if data["sID"] == -1:
-                    logger.debug("no submission found - sleep some time")
-                    time.sleep(5)
+                    # no task available
+                    if data["sID"] == -1:
+                        logger.debug("no submission found - sleep some time")
+                        time.sleep(5)
 
-                # got a task to grade:
-                else:
-                    logger.info(
-                        "==================================================")
-                    logger.info(
-                        "got submission {} to grade.".format(data["sID"]))
+                    # got a task to grade:
+                    else:
+                        logger.info(
+                            "==================================================")
+                        logger.info(
+                            "got submission {} to grade.".format(data["sID"]))
 
-                    logger.debug(
-                        "The grading-task data contains {0} properties".format(len(data)))
-                    logger.debug("\n")
-                    for key in data:
-                        logger.debug(key + " : " + str(data[key]))
-
-                    submission_id = data["sID"]
-                    assessment_id = data["aID"]
-                    allow_sorry = data["allow_sorry"]
-
-                    try:
-                        summary = self.grade_submission(submission_id, assessment_id, data["files"]["Defs"], data[
-                            "files"]["Submission"], data["files"]["Check"], data["image"], data["version"], data["timeout_socket"],
-                            data["timeout_all"], data["allow_sorry"], data["checkfile"] if "checkfile" in data else None)
-                    # In case the grader signals that the watchdog should restart the whole thing.
-                    except Grader_Panic:
-                        self.tidy()
-                        raise Watchdog_Restart()
-
-                    data = json.dumps(
-                        {'submission_is_valid': summary['submission_is_valid'], 'sID': submission_id, 'aID': assessment_id, 'msg': json.dumps(summary)})
-
-                    logger.debug("put the result back to the server")
-                    response = requests.post(
-                        self.baseurl + self.puturl, data=data, headers=self.headers)
-
-                    if(response.ok):
-                        data = json.loads(response.content)
                         logger.debug(
-                            "The response contains {0} properties".format(len(data)))
+                            "The grading-task data contains {0} properties".format(len(data)))
                         logger.debug("\n")
                         for key in data:
-                            logger.debug(key + " : " + data[key])
-                    else:
-                        response.raise_for_status()
+                            logger.debug(key + " : " + str(data[key]))
 
-            else:
-                try:
-                    response.raise_for_status()
-                except requests.HTTPError as e:
-                    logger.debug(e)
+                        submission_id = data["sID"]
+                        assessment_id = data["aID"]
+                        allow_sorry = data["allow_sorry"]
+
+                        try:
+                            summary = self.grade_submission(submission_id, assessment_id, data["files"]["Defs"], data[
+                                "files"]["Submission"], data["files"]["Check"], data["image"], data["version"], data["timeout_socket"],
+                                data["timeout_all"], data["allow_sorry"], data["checkfile"] if "checkfile" in data else None)
+                        # In case the grader signals that the watchdog should restart the whole thing.
+                        except Grader_Panic:
+                            self.tidy()
+                            raise Watchdog_Restart()
+
+                        data = json.dumps(
+                            {'submission_is_valid': summary['submission_is_valid'], 'sID': submission_id, 'aID': assessment_id, 'msg': json.dumps(summary)})
+
+                        logger.debug("put the result back to the server")
+                        response = requests.post(
+                            self.baseurl + self.puturl, data=data, headers=self.headers)
+
+                        if(response.ok):
+                            data = json.loads(response.content)
+                            logger.debug(
+                                "The response contains {0} properties".format(len(data)))
+                            logger.debug("\n")
+                            for key in data:
+                                logger.debug(key + " : " + data[key])
+                        else:
+                            response.raise_for_status()
+
+                else:
+                    try:
+                        response.raise_for_status()
+                    except requests.HTTPError as e:
+                        logger.debug(e)
+            except Exception as e:
+                logger.info("An error occured, just try again! (%s)" % e)
+                
 
             logger.debug("and start all over :)")
